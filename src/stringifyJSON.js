4fefa94f459557a1373da29232faefda9b2a43f1
riahk <moar.riah@gmail.com>
2015-02-04 16:22:16 -0800

    Works for primitives and arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //base cases for primitives (numbers, strings, booleans and null)
+  if(typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  
+  if(obj === null) {
+    return 'null';
+  }
+
+  if(typeof obj === ('boolean' || 'number')) {
+    return obj.toString();
+  }
+
+  //recursive cases for arrays and objects
+  if(Array.isArray(obj)) {
+    if(obj.length === 0) { //base case: the array is empty
+      return '[]'; 
+    } else {  
+        var stringElems = "";
+        for(var i = 0; i < obj.length; i++) {
+          stringElems += stringifyJSON(obj[i]);
+          if( i != obj.length - 1) {
+            stringElems += ',';
+          }
+        }
+      return '[' + stringElems + ']';
+    }
+  }
+  
+  if(typeof obj === 'object') {
+    if(Object.getOwnPropertyNames(obj).length === 0) { //base case: the object is empty
+      return '{}';
+    } else {
+        return null;
+      }
+  } else { return obj.toString(); } //this code should never be executed
 };
